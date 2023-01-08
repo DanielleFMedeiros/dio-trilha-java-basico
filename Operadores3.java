5395aec9fa660afcf82791110f8598f23e401a4d
Danielle Farias <Danielle_Fariass@hotmail.com>
2023-01-08 20:03:21 -0300

    operadores unario

---
+++ Operadores3.java
@@ -0,0 +1,23 @@
+public class Operadores3 {
+    public static void main(String[] args) {
+        //OPERADOR UNARIO
+
+        //int numero = 5;
+
+        //x repeticao
+       // numero++;
+       // numero = numero + 1;
+
+       // System.out.println(numero--);
+       // System.out.println(numero);
+
+       
+       boolean variavel = true;
+      
+       variavel = !variavel;
+
+       System.out.println(variavel);
+
+
+    }
+}
