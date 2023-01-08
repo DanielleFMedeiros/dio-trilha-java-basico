ac83478897288d69195c11b2fa434791e2c5b1b7
Danielle Farias <Danielle_Fariass@hotmail.com>
2023-01-08 18:54:23 -0300

    conteudo tipos de variaveis

--- TiposVariaveis.java
+++ TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
-    public static void main(String[] args) {
-        System.out.println("Hello, Word!");
+    public static void main(String[] args) throws Exception {
+        //TIPOS PRIMITIVOS
+        //ESTUDEM A CLASSE STRING QUE REPRESENTA TEXTO NA APLICAÇÃO
+     
+        String meuNome = "GLEYSON SAMPAIO";
+        double salarioMinimo = 2500;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short)numeroNormal;
+
+       int numero = 5;
+       numero = 10;
+
+       System.out.println(numero);
+
+       final double VALOR_DE_PI = 3.14;
+        
+
     }
 }
